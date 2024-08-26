af83ad2edeebc7a540ce5aeef03c56b9ae615db9
Justin Simon <justin.simon@jumo.net>
2024-08-26 16:52:22 +0200

    M

--- BaKiModell.R
+++ BaKiModell.R
@@ -9,6 +9,40 @@ file_path <- "/home/justin.simon/repos/BA/Testdaten/final_df2_cleaned.csv"
 final_df2_loaded <- read.csv(file_path, stringsAsFactors = FALSE)
 
 
+
+convert_to_numeric <- function(df) {
+	df <- as.data.frame(lapply(df, function(x) {
+		# Ersetze Kommas durch Punkte
+		x <- gsub(",", ".", x)
+		
+		# Wandle den Text in numerische Werte um
+		num_x <- as.numeric(x)
+		
+		# Überprüfe, ob die Umwandlung fehlgeschlagen ist und versuche es als numerische Umwandlung
+		if (any(is.na(num_x)) && !all(is.na(x))) {
+			num_x <- as.numeric(as.character(x))
+		}
+		
+		return(num_x)
+	}))
+	return(df)
+}
+
+# Wandle alle Spalten in numerische Werte um
+final_df2_loaded <- convert_to_numeric(final_df2_loaded)
+
+# Überprüfe die Struktur des neuen DataFrames
+str(final_df2_loaded)
+
+# Überprüfe, ob noch Nicht-Numerische Werte vorhanden sind
+any(is.na(final_df2_loaded))
+
+
+
+
+
+str(final_df2_numeric)
+
 x <- final_df2_loaded[, !names(final_df2_loaded) %in% "Gesamt_MEASURE_FAIL_CODE"]
 y <- final_df2_loaded$Gesamt_MEASURE_FAIL_CODE
 
@@ -42,3 +76,19 @@ yTestScale <- predict(scalerLabels, yTestScale)
 
 yTrain <- unlist(yTrain)
 yTestScale <- unlist(yTestScale)
+
+
+model <- train(
+	x = xTrain, 
+	y = yTrain,
+	method = 'mlp',
+	trControl = trainControl(
+		method = 'cv', 
+		number = 10, 
+		verboseIter = TRUE  # Zeigt Fortschrittsmeldungen in der Konsole an
+	),
+	tuneGrid = expand.grid(
+		size = c(100, 200, 400, 600, 800, 1000)
+	),
+	verbose = TRUE
+)
